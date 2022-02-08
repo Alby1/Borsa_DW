26bd8055fcaa33011e62fff1333d524c9704c4f9
schiavoanto <schiavoneantonio@istitutocorni.it>
2022-02-08 11:36:48 +0100

    changed API and added requester + formatter in Java

--- src/BORSAT3/src/dw/t3/Parser.java
+++ src/BORSAT3/src/dw/t3/Parser.java
@@ -7,11 +7,29 @@ public class Parser {
     public void parseJSON(String json) {
         JSONObject obj = new JSONObject(json);
 
-        JSONArray arr = obj.getJSONArray("results");
-        for (int i = 0; i < arr.length(); i++) {
-            JSONObject value = arr.getJSONObject(i);
-            System.out.println(value.toString());
+        JSONArray arr = obj.getJSONArray("data");
+        JSONObject value = arr.getJSONObject(0);
+        value.remove("adj_high");
+        value.remove("adj_low");
+        value.remove("adj_close");
+        value.remove("adj_open");
+        value.remove("adj_volume");
+        value.remove("split_factor");
+        value.remove("dividend");
+        for (int i = 1; i < arr.length(); i++) {
+            value = arr.getJSONObject(i);
+            value.remove("adj_high");
+            value.remove("adj_low");
+            value.remove("adj_close");
+            value.remove("adj_open");
+            value.remove("adj_volume");
+            value.remove("split_factor");
+            value.remove("dividend");
+            value.remove("symbol");
+            value.remove("exchange");
+            System.out.println(value);
         }
+        System.out.println(arr);
     }
 
 }
--- src/BORSAT3/src/dw/t3/Requester.java
+++ src/BORSAT3/src/dw/t3/Requester.java
@@ -4,12 +4,15 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.time.LocalDate;
 
 public class Requester {
 
     public String makeAPIRequest() {
         try {
-            URL url = new URL("https://api.polygon.io/v2/aggs/ticker/AAPL/range/5/day/2020-06-01/2020-06-17?apiKey=EobVpUWh_Fk9qu1gQ818mOPJPJ3joOUR");
+            LocalDate to = LocalDate.of(LocalDate.now().getYear(), LocalDate.now().getMonth(), LocalDate.now().getDayOfMonth());
+            LocalDate from = to.minusYears(1);
+            URL url = new URL("http://api.marketstack.com/v1/eod?access_key=ad6efcf74f8bee5e5615cc2146452418&symbols=NFLX&limit=500&date_from=" + from + "&date_to=" + to);
             HttpURLConnection con = (HttpURLConnection) url.openConnection();
             con.setRequestMethod("GET");
             BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
